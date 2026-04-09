4c0cf3f17c398c2a65430e059c8a9ce9b7fd3b54
sam09-bit <moryasameer59@gmail.com>
2026-04-09 23:04:07 +0530

    change

---
+++ server/__init__.py
@@ -0,0 +1,2 @@
+# This file can be completely empty. 
+# Its mere existence tells Python that the 'server' folder is a package!
--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
 from openenv.core.env_server import create_fastapi_app
-from server.environment import FounderEnvironment
-from founder_endurance.models import FounderAction, FounderObservation
+from .environment import FounderEnvironment  # Notice the dot here!
+from models import FounderAction, FounderObservation
 
 app = create_fastapi_app(FounderEnvironment, FounderAction, FounderObservation)
